f573cce76d651d61c11c8509866e61a4705946ed
vatsal <vatsal.acct01@gmail.com>
2021-07-07 11:49:22 +0530

    add nav and create pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Navs from './components/Navs';
+import Home from './components/pages/Home';
+import Starred from './components/pages/Starred';
 
 function App() {
-  return <div>hello</div>;
+  return (
+    <div>
+      <Navs />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+
+        <Route exact path="/starred">
+          <Starred />
+        </Route>
+
+        <Route path="/">
+          <div>Page Not Found (404)</div>
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/pages/Starred.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Starred = () => <div>Starred Page</div>;
+
+export default Starred;
