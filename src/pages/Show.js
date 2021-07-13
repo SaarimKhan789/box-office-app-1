93a9b1659576a5cf6436e9a95213e254c5209472
vatsal <vatsal.acct01@gmail.com>
2021-07-13 17:38:59 +0530

    created show page  components and input data in it

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,5 +1,10 @@
+/* eslint-disable no-underscore-dangle */
 import React, { useEffect, useReducer } from 'react';
 import { useParams } from 'react-router-dom';
+import Cast from '../components/show/Cast';
+import Details from '../components/show/Details';
+import Seasons from '../components/show/Seasons';
+import ShowMainData from '../components/show/ShowMainData';
 import { apiGet } from '../misc/config';
 
 const reducer = (prevState, action) => {
@@ -58,6 +63,32 @@ const Show = () => {
   }
   console.log(show);
 
-  return <div>This is show</div>;
+  return (
+    <div>
+      <ShowMainData
+        name={show.name}
+        rating={show.rating}
+        summary={show.summary}
+        tags={show.genres}
+        image={show.image}
+      />
+      <div>
+        <h2>Details</h2>
+        <Details
+          status={show.status}
+          premiered={show.premiered}
+          network={show.network}
+        />
+      </div>
+      <div>
+        <h2>Seasons</h2>
+        <Seasons seasons={show._embedded.seasons} />
+      </div>
+      <div>
+        <h2>Cast</h2>
+        <Cast cast={show._embedded.cast} />
+      </div>
+    </div>
+  );
 };
 export default Show;
